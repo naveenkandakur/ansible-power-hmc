408d15afb794c571a24756c312630d2b790a03a0
SreenidhiS1 <155409322+SreenidhiS1@users.noreply.github.com>
2025-10-08 12:51:05 +0530

    Update hmc_rest_client.py

--- plugins/module_utils/hmc_rest_client.py
+++ plugins/module_utils/hmc_rest_client.py
@@ -529,7 +529,7 @@ class HmcRestClient:
                             force_basic_auth=True,
                             timeout=300)
             if resp.code != 200:
-                logger.debug(f"Request failed with response code: {resp.code}")
+                logger.debug("Request failed with response code: %s", resp.code)
                 return None
             response = resp.read()
             dom = xml_strip_namespace(response)
